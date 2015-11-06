5b6b9d252f7378a952b9a93cfc63973ad41d2b7b
Tomefaire <andy.h.nguyen1996@gmail.com>
2015-11-06 11:19:28 -0500

    Smooth Camera Rotation Added

--- Assets/Scripts/View Model Component/CameraGUIHandler.cs
+++ Assets/Scripts/View Model Component/CameraGUIHandler.cs
@@ -2,11 +2,35 @@
 using System.Collections;
 
 public class CameraGUIHandler : MonoBehaviour {
-    [SerializeField] GameObject selectedCamera;
 
-    public void RotateCamera()
+    public void Start()
     {
-        selectedCamera.transform.Rotate(0, +90, 0);
+        currentAngle = transform.eulerAngles;
+        targetAngle = transform.eulerAngles;
     }
 
+    Vector3 targetAngle;
+
+    Vector3 currentAngle;
+
+    bool lerp;
+
+    public void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lerp = true;
+            targetAngle.y += 90;
+        }
+        if (lerp)
+        {
+            currentAngle = new Vector3(
+                Mathf.LerpAngle(currentAngle.x, targetAngle.x, Time.deltaTime),
+                Mathf.LerpAngle(currentAngle.y, targetAngle.y, Time.deltaTime),
+                Mathf.LerpAngle(currentAngle.z, targetAngle.z, Time.deltaTime));
+
+            transform.eulerAngles = currentAngle;
+        }
+    }
 }
